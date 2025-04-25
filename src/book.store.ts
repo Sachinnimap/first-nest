e598d9f1f5a08aced4249b2404f808008ebb0367
sachin <sachin.ram@nimapinfotech.com>
2025-04-25 14:14:00 +0530

    injectable types DEFAULT REQUEST TRANSIENT

--- src/book.store.ts
+++ src/book.store.ts
@@ -1,6 +1,9 @@
-import { Injectable ,Get} from "@nestjs/common";
+import { Injectable ,Get, Scope} from "@nestjs/common";
 
-@Injectable()
+// scope:Scope.DEFAULT  - means work as normal Inject
+//scope:Scope.REQUEST  - create new instance for controller and himself for every request //GET , PUT, POST,DELETE--
+// scope:Scope.TRANSIENT  - create new different-different instance for differet -different controllers those are using this INJECTABLE calss!
+@Injectable()   //{scope:Scope.DEFAULT} we can alos pass this!
 export class  BookStore{
     private store = new Map<number,string>()
         Books : string[] = ['fist_BOOK','seocnd_BOOK','third_BOOK','fourth_BOOK','fifth_BOOK','sixth_BOOK','seventh_BOOK','eighth']
