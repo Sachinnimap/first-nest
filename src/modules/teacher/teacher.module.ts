286a8aa4a4b7fa586e979bcc6b7d4c8adbcfa821
sachin <sachin.ram@nimapinfotech.com>
2025-04-26 11:28:15 +0530

    gloabally available module!

--- src/modules/teacher/teacher.module.ts
+++ src/modules/teacher/teacher.module.ts
@@ -1,9 +1,9 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import {TeacherController} from './controllers/teacher.controller'
 import {ActiveTeacherController} from "./controllers/activeTeacher.controller"
 import {TeacherService} from  "./services/teacher.service"
 
-
+@Global() // so this will available for globally so anyone can use it
 @Module({
     imports:[], //import some modules which we can use in this module!
     controllers: [ActiveTeacherController,TeacherController],
